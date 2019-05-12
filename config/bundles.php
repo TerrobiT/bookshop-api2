f3bf3bc186e30d4f0736f655fd08dc816656f241
terrobit <terrobitm@gmail.com>
2019-05-12 20:15:38 +0300

    Add JWT

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
